b5c8c8d8116ac6bf39e33ed25092af06bce557e6
Barendan <barendan@gmail.com>
2024-01-18 00:29:17 -0500

    completed cleaning of garbage

--- src/App.js
+++ src/App.js
@@ -11,23 +11,6 @@ import Navbar from "./Components/Navbar";
 import Products from "./Components/Products";
 import Services from "./Components/Services";
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAZ-pDLBFq08WBq3EKyxfQW4nhP7nOIHHo",
-//   authDomain: "esperanza-a2eb5.firebaseapp.com",
-//   projectId: "esperanza-a2eb5",
-//   storageBucket: "esperanza-a2eb5.appspot.com",
-//   messagingSenderId: "405249126239",
-//   appId: "1:405249126239:web:c1dc6b68524be33b2590ea",
-//   measurementId: "G-8PEXWYG0T7"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 function App() {
   useEffect(() => {
