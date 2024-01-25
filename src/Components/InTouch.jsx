e00abb039bcf9aa2223dce1e2fd08cbf88f605c2
Barendan <barendan@gmail.com>
2024-01-25 14:47:45 -0500

    setup emailjs and create contact form functionality

--- src/Components/InTouch.jsx
+++ src/Components/InTouch.jsx
@@ -1,18 +1,102 @@
-// import mapImg from "../Assets/map-img.svg";
-// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+
 import phoneIcon from "../Assets/phone-icon.svg";
 import locationIcon from "../Assets/location-icon.svg";
 import mailIcon from "../Assets/mail-icon.svg";
+
 // import bg1 from "../Assets/touch-bg-1.svg";
 import bg2 from "../Assets/touch-bg-2.svg";
 import bg3 from "../Assets/touch-bg-3.svg";
 // import bg4 from "../Assets/touch-bg-4.svg";
 // import bg5 from "../Assets/touch-bg-5.svg";
 
+
+
 function InTouch() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormErrors({ ...formErrors, [e.target.name]: "" });
+  };
+ 
+  const sendEmail = () => {
+    const userID = "o7sxEIHoem1uxKznK";
+    const templateID = "template_3kyyb2l";
+
+    const templateParams = {
+      from_name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+      to_name: "Danielbarenboim",
+      message: formData.message,
+    };
+
+    return emailjs.send("service_h8m6hbq", templateID, templateParams, userID);
+  };
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!formData.name.trim()) {
+      errors.name = "Name is required";
+      isValid = false;
+    }
+
+    if (!formData.phone.trim()) {
+      errors.phone = "Phone number is required";
+      isValid = false;
+    }
+    
+    if (!formData.email.trim()) {
+      errors.email = "Email is required";
+      isValid = false;
+    }
+    if (!formData.message.trim()) {
+      errors.message = "Message is required";
+      isValid = false;
+    }
+    setFormErrors(errors);
+    return isValid;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
+    try {
+      await sendEmail();
+      toast.success("Mail sent successfully!");
+
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+    } catch (error) {
+      console.error("Email could not be sent:", error);
+    }
+  };
+
+
   
   return (
     <>
+      <ToastContainer />
+
       <div id="inTouchSection" className="flex justify-center items-center py-20 px-4 sm:px-6 md:px-8 lg:px-24">
 
         <div className="absolute flex justify-between w-full mt-[-1431px] lg:mt-[-714px]">
@@ -21,23 +105,27 @@ function InTouch() {
           <img className="hidden sm:block opacity-[0.3] z-[-1]" src={bg3} alt="_bg" />
         </div>
 
+
+
+
         <div 
           // data-aos="flip-up" 
-          data-aos-delay="200" 
+          // data-aos-delay="200" 
           className="flex flex-col relative w-[90%]"
         >
 
           <div className="flex flex-col lg:flex-row">
             
-            <div className="w-full lg:w-[70%] bg-[#EDEBE4] rounded-[20px] lg:rounded-tr-[0px] lg:rounded-br-[0px] flex flex-col gap-10 p-20 py-6 lg:pr-[200px] xl:pr-[300px]">
+            <div className="w-full lg:w-[70%] bg-[#EDEBE4] pb-[10rem] md:pb-0 rounded-[20px] lg:rounded-tr-[0px] lg:rounded-br-[0px] flex flex-col gap-8 p-3 md:p-20 lg:pr-[200px] xl:pr-[300px]">
               
-              <div className="flex flex-col gap-8">
+              {/* Intro Text and Form */}
+              <div className="flex flex-col gap-8 px-6">
                 
                 <div className="flex flex-col gap-3">
                   <p className="text-[#232323] text-[47px] fontAbril font-medium leading-normal ">
                     Get in <span className="text-[#2E4630]"></span>Touch
                   </p>
-                  <p className="text-[#666] fontQuicksand leading-normal text-lg">
+                  <p className="text-[#666] fontQuicksand leading-normal text-md md:text-lg">
                     We would love to make you feel amazing. Contact us and let's make it happen!
                   </p>
                 </div>
@@ -47,73 +135,103 @@ function InTouch() {
                     type="text"
                     placeholder="Name*"
                     className="border border-[#DFD8CA] rounded-[10px] py-3 px-4 bg-transparent"
+                    onChange={handleChange}
+                    name="name"
+                    value={formData.name}
                   />
                   <input
                     type="email"
                     placeholder="Email"
                     className="border border-[#DFD8CA] rounded-[10px] py-3 px-4 bg-transparent"
+                    onChange={handleChange}
+                    name="email"
+                    value={formData.email}
                   />
                   <input
                     type="tel"
                     placeholder="Phone number*"
                     className="border border-[#DFD8CA] rounded-[10px] py-3 px-4 bg-transparent"
+                    onChange={handleChange}
+                    name="phone"
+                    value={formData.phone}
                   />
                   <textarea
                     placeholder="Message"
                     className="border border-[#DFD8CA] rounded-[10px] py-3 px-4 bg-transparent h-[152px]"
+                    onChange={handleChange}
+                    name="message"
+                    value={formData.message}
                   />
-                  <button onClick={() => console.log("Send!")} className="rounded-full text-white bg-[#2E4630] p-3 fontQuicksand text-sm leading-normal hover:shadow-md cursor-pointer ">
-                    Send
+
+                  {formErrors.message && <p className="text-red-500">{formErrors.message}</p>}
+
+                  <button 
+                    onClick={handleSubmit}
+                    className="rounded-full text-white bg-[#2E4630] p-3 fontQuicksand text-md leading-normal hover:shadow-md cursor-pointer"
+                  >
+                    Send Message
                   </button>
+                
                 </div>
+
               </div>
+
               
-              <div className="flex gap-10 justify-start flex-wrap items-center">
+              {/* Contact Information */}
+              <div className="flex gap-6 justify-start flex-wrap items-center pb-20 px-10">
 
                 <div className="flex gap-3 justify-start items-center">
 
-                  <img src={phoneIcon} alt="_phone" />
+                  <img src={phoneIcon} className="md:hidden" width={42} height={42} alt="_phone" />
+                  {/* <img src={phoneIcon} 
+                  className="hidden md:block" 
+                  alt="_phone" /> */}
 
                   <div className="fex flex-col gap-1">
-                    <p className="fontMont font-medium text-sm leading-normal text-[#2E4630]">
+                    <p className="fontMont font-medium text-lg leading-normal text-[#2E4630]">
                       Phone
                     </p>
-                    <p className="fontMont text-sm leading-normal text-[#666]">
-                      03 5432 1234
+                    <p className="fontMont text-md leading-normal text-[#666]">
+                      (305) 306 7522
                     </p>
                   </div>
 
                 </div>
 
                 <div className="flex gap-3 justify-start items-center">
-                  <img src={locationIcon} alt="_location" />
+                  
+                  <img src={mailIcon} className="md:hidden" width={42} height={42} alt="_email" />
+                  <img src={mailIcon} className="hidden md:block" alt="_email" />
 
                   <div className="fex flex-col gap-1">
-                    <p className="fontMont font-medium text-sm leading-normal text-[#2E4630]">
-                      Location
-                    </p>
-                    <p className="fontMont text-sm leading-normal text-[#666]">
-                      03 5432 1234
-                    </p>
-                  </div>
-
-                </div>
-
-                <div className="flex gap-3 justify-start items-center">
-                  <img src={mailIcon} alt="_email" />
-
-                  <div className="fex flex-col gap-1">
-                    <p className="fontMont font-medium text-sm leading-normal text-[#2E4630]">
+                    <p className="fontMont font-medium text-lg leading-normal text-[#2E4630]">
                       Email
                     </p>
-                    <p className="fontMont text-sm leading-normal text-[#666]">
-                      info@marcc.com.au
+                    <p className="fontMont text-md leading-normal text-[#666]">
+                      stephaniesperanzaskin@gmail.com
+                    </p>
+                  </div>
+
+                </div>
+
+                <div className="flex gap-3 justify-start items-center">
+                  
+                  <img src={locationIcon} className="md:hidden" width={42} height={42} alt="_location" />
+                  <img src={locationIcon} className="hidden md:block" alt="_location" />
+
+                  <div className="fex flex-col gap-1">
+                    <p className="fontMont font-medium text-lg leading-normal text-[#2E4630]">
+                      Location
+                    </p>
+                    <p className="fontMont text-md leading-normal text-[#666]">
+                      8275 W 12th Ave, Hialeah, Florida
                     </p>
                   </div>
 
                 </div>
 
               </div>
+              
             </div>
 
 
@@ -121,9 +239,36 @@ function InTouch() {
                 <img className='opacity-[0.3]' src={bg5} alt="_bg"/>
             </div> */}
 
-            <div className="w-full lg:w-[30%]  bg-[#2E4630] rounded-[20px] lg:rounded-tl-[0px] lg:rounded-bl-[0px] flex justify-center items-center py-80 lg:py-0">
+            {/* <div className="w-full lg:w-[30%]  bg-[#2E4630] rounded-[20px] lg:rounded-tl-[0px] lg:rounded-bl-[0px] flex justify-center items-center py-80 lg:py-0"> */}
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3589.135994871039!2d-80.31046002404587!3d25.897899502962797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9bace5d1235b1%3A0x40b5cb76ccd4410a!2sEsperanza%20Skin%20Care!5e0!3m2!1sen!2sus!4v1705526970563!5m2!1sen!2sus" title="Esperanza Location" width="100%" height="519" style={{border:0,borderRadius:10,position:'absolute', maxWidth: "404px"}} className="lg:left-[55%]" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+
+            {/* Two google maps, one for desktop and one for mobile */}
+            <div className="w-full lg:w-[30%] bg-[#2E4630] rounded-bl-[20px] rounded-br-[20px] lg:rounded-tl-[0px] lg:rounded-bl-[0px] flex justify-center items-center py-20 md:py-80 lg:py-0">
+
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3589.135994871039!2d-80.31046002404587!3d25.897899502962797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9bace5d1235b1%3A0x40b5cb76ccd4410a!2sEsperanza%20Skin%20Care!5e0!3m2!1sen!2sus!4v1705526970563!5m2!1sen!2sus" 
+                title="Esperanza Location" 
+                width="336px"
+                height="280px"
+                style={{ border: 0, borderRadius: 10, position: "absolute", maxWidth: "404px", top: "71%" }}
+                className="md:hidden"
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade"
+              /> 
+
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3589.135994871039!2d-80.31046002404587!3d25.897899502962797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9bace5d1235b1%3A0x40b5cb76ccd4410a!2sEsperanza%20Skin%20Care!5e0!3m2!1sen!2sus!4v1705526970563!5m2!1sen!2sus" 
+                title="Esperanza Location" 
+                width="100%"
+                height="519"
+                style={{ border: 0, borderRadius: 10, position: "absolute", maxWidth: "404px" }}
+                className="hidden md:block lg:left-[55%]"
+                allowfullscreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              />
+
             </div>
 
           </div>
